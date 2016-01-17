47c769e7da4f99f72b6c230ff4f3654695d984b7
paulo <paulorec@gmail.com>
2016-01-17 17:03:28 -0300

    Commiting JS

---
+++ README.md
@@ -0,0 +1,1 @@
+# ui-sortable-header
--- js/ui-sortable-header.js
+++ js/ui-sortable-header.js
@@ -3,14 +3,54 @@
   "use strict";
 
   angular
-  .module("sortableHeader")
-  .directive("sortableGrouper", SortableGrouper)
-  .directive("sortableField", SortableField)
+  .module("uiSortableHeader", [])
+  .directive("uiSortableGroup", SortableGroup)
+  .directive("uiSortableField", SortableField);
 
-  function SortableGrouper() {
+  function SortableGroup() {
 
+    var controller = function($scope, $attrs) {
+
+      var vm = this;
+
+      vm.predicate = null;
+      vm.asc = true;
+
+      vm.refresh = $attrs.uiSortableGroup || function() {};
+
+      vm.setPredicate = function(predicate) {
+
+        vm.predicate = predicate;
+
+        _refresh();
+      };
+
+      vm.getPredicate = function() {
+
+        return vm.predicate;
+      };
+
+      vm.setAsc = function(asc) {
+
+        vm.asc = asc;
+
+        _refresh();
+      };
+
+      vm.getAsc = function() {
+        return vm.asc;
+      }
+
+      function _refresh() {
+
+        $scope.$broadcast("ui-sortable-group-change", {});
+        vm.refresh(vm.predicate, vm.asc);
+
+      }
+    };
     var directive = {
-      restrict : "AE"
+      restrict : "A"
+      ,controller : controller
     }
 
     return directive;
@@ -18,6 +58,47 @@
 
   function SortableField() {
 
+    var directive = {
+        restrict : "A"
+        ,require : "^uiSortableGroup"
+        ,link: function(scope, element, attrs, uiSortableGroup) {
+
+          var onClick = function(event) {
+
+            var predicate = attrs.uiSortableField;
+            var asc = (predicate === uiSortableGroup.getPredicate()) ? !uiSortableGroup.getAsc() : true;
+
+            uiSortableGroup.setPredicate(predicate);
+            uiSortableGroup.setAsc(asc);
+
+          };
+
+          element.bind('click', onClick);
+          element.addClass('ui-sortable-field');
+
+          scope.$on("ui-sortable-group-change", function(event) {
+
+            element.removeClass('ui-sortable-field-asc');
+            element.removeClass('ui-sortable-field-desc');
+
+            if(uiSortableGroup.getPredicate() != attrs.uiSortableField) {
+
+              //nop
+            } else if(uiSortableGroup.getAsc())
+
+              element.addClass('ui-sortable-field-asc');
+
+            else {
+
+              element.addClass('ui-sortable-field-desc');
+            }
+
+          });
+
+        }
+    }
+
+    return directive;
   }
 
 }(window.angular));
